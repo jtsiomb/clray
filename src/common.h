a9026edf941bf498f641d0508d69a5d1dd01f6f6
John Tsiombikas <none@none>
2010-08-31 01:03:46 +0100

    forgot to add common.h to the repository

---
+++ src/common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+/* primary ray magnitude */
+#define RAY_MAG		500.0
+
+/* maximum faces per leaf node of the kd-tree */
+#define MAX_NODE_FACES		32
+
+/* maximum kdtree depth */
+#define MAX_TREE_DEPTH		64
+
+/* width in pixels of the image-ified kdtree node */
+#define KDIMG_NODE_WIDTH	16
+
+/* maximum kdtree image height */
+#define KDIMG_MAX_HEIGHT	4096
+
+#endif	/* COMMON_H_ */
